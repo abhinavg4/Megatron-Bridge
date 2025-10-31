992f836647417e46ecf8fcc5950d221959bcb2cb
Huy Vu2 <huvu@login-eos02.eos.clusters.nvidia.com>
2025-10-31 07:05:26 -0700

    clean up, remove all CP for sbhd, CP now is only for thd

--- src/megatron/bridge/models/wan/wan_provider.py
+++ src/megatron/bridge/models/wan/wan_provider.py
@@ -46,9 +46,7 @@ class WanModelProvider(TransformerConfig, ModelProviderMixin[VisionModule]):
     parallel_output: bool = True
     bf16: bool = False
     params_dtype: torch.dtype = torch.float32
-    # DEBUGGING thd
-    # qkv_format: str = 'sbhd'
-    qkv_format: str = 'thd'
+    qkv_format: str = "sbhd" # "thd". NOTE: if we use context parallelism, we need to use "thd"
     # these attributes are unused for images/videos, we just set because bridge training requires for LLMs
     seq_length: int = 1024
     share_embeddings_and_output_weights: bool = False
